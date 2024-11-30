3cbba9d56cd33e88f5aa53cf85306ff7b52ab90f
glebishead <glebaskayumov@yandex.ru>
2024-11-30 16:30:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,20 +6,19 @@ int main() {
     int x, i;
     int k = 1;
     
-    std::cout << "Введите x. Число для изменений" << std::endl;
+    std::cout << "Enter x" << std::endl;
     std::cin >> x;
     
-    std::cout << "Введите i. i-ый бит числа " << x << " изменится на 0." << std::endl;
+    std::cout << "Enter i" << std::endl;
     std::cin >> i;
     
     k = k << i;
     
-    std::cout << "Число " << x << " в двоичном виде " << std::endl;
-    std::cout << std::bitset<sizeof(int) * 8>(x) << std::endl;
+    std::cout << x << " is " << std::endl << std::bitset<sizeof(int) * 8>(x) << " binary" << std::endl;
     
     x = x & ~k;
-    std::cout << "Результат" << std::endl;
-    std::cout << std::bitset<sizeof(int) * 8>(x) << std::endl;
+    std::cout << "Result - " << x << " is " << std::endl;
+    std::cout << std::bitset<sizeof(int) * 8>(x) << " binary" << std::endl;
     // std::cout << std::bitset<sizeof(int) * 8>(k) << std::endl;
     return 0;
 }
